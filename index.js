38b646e1aabce597a88ad1a26589349c96ddf0fd
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-11 11:57:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function takeANumber(line, current) {
   line.push(current)
-  let lineNum = line.length
-  console.log(`Welcome, ${current}. You are number ${line[lineNum]} in line.`)
+  console.log(`Welcome, ${current}. You are number ${line[line.length]} in line.`)
   return line
 }
