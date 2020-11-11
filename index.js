42979347c92ef181916e69778071371620b97b9a
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-11 12:05:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function takeANumber(line, current) {
   
   return `Welcome, ${current}. You are number ${line.length} in line.`
 }
+
+
+function nowServing(line) {
+  
+   if (line.length > 0) {
+   let temp = line[0]
+   line.shift()
+    return temp
+  } 
+  
+  else {
+    return ''
+  }
+}
